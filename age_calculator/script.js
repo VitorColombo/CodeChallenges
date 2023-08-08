7444d22f3228dcbf3551ea96f8918f880a438324
VitorColombo <vitorcolombonunes@gmail.com>
2023-08-08 02:36:09 -0300

    errorTreatment inputs

--- age_calculator/script.js
+++ age_calculator/script.js
@@ -1,11 +1,46 @@
 
+
 function calculateAge() {
+    document.getElementById("dayError").innerHTML = "";
+    document.getElementById("monthError").innerHTML = "";
+    document.getElementById("yearError").innerHTML = "";
     const day = document.getElementById("day").value;
     const month = document.getElementById("month").value;
     const year = document.getElementById("year").value;
 
     const now = new Date();
     const birth = new Date(year, month - 1, day);
+
+    if (!day && !month && !year) {
+        document.getElementById("dayError").innerHTML = "This field is required";
+        document.getElementById("monthError").innerHTML = "This field is required";
+        document.getElementById("yearError").innerHTML = "This field is required";
+        document.getElementById("day").classList.add("error"); // Add error class
+        document.getElementById("month").classList.add("error"); // Add error class
+        document.getElementById("year").classList.add("error"); // Add error class
+
+        return; // Exit the function if there's an error
+    }
+    
+    if (!day) {
+        document.getElementById("dayError").innerHTML = "Day is required";
+        document.getElementById("day").classList.add("error"); // Add error class
+
+        return; // Exit the function if there's an error
+    }
+
+    if (!month) {
+        document.getElementById("monthError").innerHTML = "Month is required";
+        document.getElementById("month").classList.add("error"); // Add error class
+        return; // Exit the function if there's an error
+    }
+
+    if (!year) {
+        document.getElementById("yearError").innerHTML = "Year is required";
+        document.getElementById("year").classList.add("error"); // Add error class
+        return; // Exit the function if there's an error
+    }
+
 
     let ageInMilliseconds = now - birth;
 
@@ -34,3 +69,4 @@ function getYear() {
     document.getElementById("year").max = year;
     return year;
   }
+  getYear();
